171b1ef65e2874b3511d2bdd9a62d6dbfafed4f8
Yasmin <85323320+YasminSousaa@users.noreply.github.com>
2026-05-14 14:39:24 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+
+    const evento = {
+        nome: 'Copa do Mundo 2026 - Evento PaaS',
+        local: 'São Paulo - Brasil',
+        data: '15 de Julho de 2026',
+        horario: '19:00',
+        descricao:
+            'Evento promovido em plataforma PaaS para integração de seleções da Copa do Mundo 2026.',
+        cadastro:
+            'http://localhost:8080'
+    }
+
+    res.render('index', { evento })
+})
+
+app.listen(3000, () => {
+    console.log('Servidor rodando em http://localhost:3000')
+})
